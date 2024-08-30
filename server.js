76cc46b4624fafe919ec4797fba38f35e98790e1
Ritchie Dimaria <rdimaria@Ritchies-MacBook-Air.local>
2024-08-30 16:41:52 -0400

    Edited to listed on IPv6 private network

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ app.post('/generate-component', upload.single('image'), async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '::',() => {
+  console.log(`Server running on port [::]${PORT}`);
 });
