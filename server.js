eec9b8c8487a1277fb994b1caad1098e66d27b93
Ritchie Dimaria <rdimaria@Ritchies-MacBook-Air.local>
2024-09-18 15:32:10 -0400

    Added Database Functionality to backend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const axios = require('axios');
+const { Pool } = require('pg');
 require('dotenv').config();
 
 
